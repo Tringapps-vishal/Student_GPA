2754a590c045d6e596ed471dbb04103d55c52f39
Tringapps-vishal <vishal.k@tringapps.com>
2023-03-17 12:34:48 +0530

    commit

--- src/main/java/gpamain/Main.java
+++ src/main/java/gpamain/Main.java
@@ -5,15 +5,13 @@ import java.util.logging.Logger;
 import calculation.*;
 import gpaupdate.Update;
 public class Main {
-    public static String name;
     public static void main(String[] arg)
     {
-
         final Logger log= Logger.getLogger("Main");
         Scanner sc=new Scanner(System.in);
         log.info("-------NEW STUDENT DETAILS-----");
         log.info("Enter your name:");
-        name=sc.nextLine();
+        String name=sc.nextLine();
         log.info("Enter your grade:");
         String grade=sc.nextLine();
         Calculation ca=new Calculation(name);
