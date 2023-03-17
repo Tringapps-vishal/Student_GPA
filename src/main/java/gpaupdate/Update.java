56607c7c847ad7eecce99fae158447a771c3b7ff
Tringapps-vishal <vishal.k@tringapps.com>
2023-03-17 12:22:24 +0530

    commit

--- src/main/java/gpaupdate/Update.java
+++ src/main/java/gpaupdate/Update.java
@@ -5,11 +5,11 @@ import java.util.Scanner;
 import java.util.logging.Logger;
 public class Update {
     Logger log=Logger.getLogger("Update");
+    Scanner sc=new Scanner(System.in);
     Calculation ca=new Calculation(null);
     public void updateGpa()
     {
         int choice;
-        Scanner sc=new Scanner(System.in);
         log.info("Do you need to update your grade  1.YES  2.NO");
         choice=sc.nextInt();
         if(choice==1)
